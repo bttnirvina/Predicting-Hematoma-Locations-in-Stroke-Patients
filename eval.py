09a2dbacef980567b76bc33e14ba5650aafac259
Colin D'Amore <cdamore@ualberta.ca>
2019-03-27 12:19:53 -0600

    add FCN

---
+++ eval.py
@@ -0,0 +1,63 @@
+from net import FCN8
+import numpy as np
+
+# load data
+X = [np.zeros((512,512,3)), np.zeros((512,512,3)), np.zeros((512,512,3)),np.zeros((512,512,3))]
+Y = [np.zeros((512,512,1)), np.zeros((512,512,1)), np.zeros((512,512,1)), np.zeros((512,512,1))]
+X, Y = np.array(X) , np.array(Y)
+
+# load model, default input size 512x512
+model = FCN8()
+model.summary()
+
+# Split between training and testing data
+from sklearn.utils import shuffle
+train_rate = 0.85
+index_train = np.random.choice(X.shape[0],int(X.shape[0]*train_rate),replace=False)
+index_test  = list(set(range(X.shape[0])) - set(index_train))
+
+# shuffle data for evalulation
+X, Y = shuffle(X,Y)
+X_train, y_train = X[index_train],Y[index_train]
+X_test, y_test = X[index_test],Y[index_test]
+print(X_train.shape, y_train.shape)
+print(X_test.shape, y_test.shape)
+
+# train
+from keras import optimizers
+sgd = optimizers.SGD(lr=1E-2, decay=5**(-4), momentum=0.9, nesterov=True)
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer=sgd,
+              metrics=['accuracy'])
+
+hist1 = model.fit(X_train,y_train,
+                  validation_data=(X_test,y_test),
+                  batch_size=32,epochs=200,verbose=2)
+
+
+# test
+y_pred = model.predict(X_test)
+y_predi = np.argmax(y_pred, axis=3)
+y_testi = np.argmax(y_test, axis=3)
+print(y_testi.shape,y_predi.shape)
+
+
+# https://fairyonice.github.io/Learn-about-Fully-Convolutional-Networks-for-semantic-segmentation.htmlx
+def IoU(Yi,y_predi):
+    ## mean Intersection over Union
+    ## Mean IoU = TP/(FN + TP + FP)
+
+    IoUs = []
+    Nclass = int(np.max(Yi)) + 1
+    for c in range(Nclass):
+        TP = np.sum( (Yi == c)&(y_predi==c) )
+        FP = np.sum( (Yi != c)&(y_predi==c) )
+        FN = np.sum( (Yi == c)&(y_predi != c))
+        IoU = TP/float(TP + FP + FN)
+        print("class {:02.0f}: #TP={:6.0f}, #FP={:6.0f}, #FN={:5.0f}, IoU={:4.3f}".format(c,TP,FP,FN,IoU))
+        IoUs.append(IoU)
+    mIoU = np.mean(IoUs)
+    print("_________________")
+    print("Mean IoU: {:4.3f}".format(mIoU))
+
+IoU(y_testi,y_predi)
